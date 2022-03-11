06590e938dea3e38ebfba668a16ffdc660ac1420
Anirban20001962 <anirbandhakuria@gmail.com>
2022-03-11 18:02:38 +0530

    Basic Structure

---
+++ springapp/src/test/java/com/example/riding/RidingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
