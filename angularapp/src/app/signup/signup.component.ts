8af29d3ae5de58165b8c8c7a52850452aa21faff
Anirban20001962 <anirbandhakuria@gmail.com>
2022-03-18 19:51:42 +0530

    login signup design done

--- angularapp/src/app/signup/signup.component.ts
+++ angularapp/src/app/signup/signup.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  signupForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    document.body.style.backgroundColor = '#D9D9D9';
+    this.signupForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      mobileNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(10),
+          Validators.minLength(10),
+        ],
+      ],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+    });
   }
-
 }
