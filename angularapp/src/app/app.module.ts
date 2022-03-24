8dce6f2b8221ff17d74e8e4c6008e9cd47fef7c5
Anirban20001962 <anirbandhakuria@gmail.com>
2022-03-24 10:01:31 +0530

    structred the ui

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { SignupComponent } from './signup/signup.component';
-import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/user/home/home.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { APIInterceptor } from './APIInterceptor';
-import { UserGuard } from './user.guard';
+import { APIInterceptor } from './utils/APIInterceptor';
+import { UserGuard } from './guard/user.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
--- angularapp/src/app/services/auth.service.ts
+++ angularapp/src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { roles } from './values';
+import { roles } from '../utils/values';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
