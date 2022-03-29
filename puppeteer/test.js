11ecd7c3a5eb4b871d76ec638926d6ba6b0c844c
Anirban20001962 <anirbandhakuria@gmail.com>
2022-03-29 08:10:16 +0530

    front end tests passed

--- puppeteer/test.js
+++ puppeteer/test.js
@@ -1,121 +1,109 @@
 const puppeteer = require('puppeteer');
+// https://8081-dbcebacafbefccbefceeaecbbfabeaecfaaccbd.examlyiopb.examly.io/
+// https://8081-dbcebacafbefccbefceeaecbbfabeaecfaaccbd.examlyiopb.examly.io/
+// https://8081-dbcebacafbefccbefceeaecbbfabeaecfaaccbd.examlyiopb.examly.io/
+// https://8081-dbcebacafbefccbefceeaecbbfabeaecfaaccbd.examlyiopb.examly.io/
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    try{
-    await page.goto('https://8081-dbcebacafbefccbefceeaecbbfabeaecfaaccbd.examlyiopb.examly.io/');
-    await page.setViewport({
-      width:1200,
-      height:800,
-    })
-      await page.click('#signupLink');
-      await page.waitForNavigation();
-      await page.type('#email', 'test@gmail.com');
-      await page.type('#username', 'testuser');
-      await page.type('#mobileNumber', '9876543210');
-      await page.type('#password', 'Test@123');
-      await page.type('#confirmPassword', 'Test@123');
-      await page.click('#submitButton');
-      await page.waitForNavigation();
-      await page.waitForSelector('#loginBox',{timeout:3000});
-      console.log('TESTCASE:FE_Signup:success');
-    }
-     catch(e){
-      console.log('TESTCASE:FE_Signup:failure');
-    }finally{
-      await page.close();
-      await browser.close();
-    }
-    
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	try {
+		await page.goto('http://localhost:8081/');
+		await page.setViewport({
+			width: 1200,
+			height: 800,
+		});
+		page.click('#signupLink');
+		await page.waitForNavigation({ timeout: 2000 });
+		await page.waitForSelector('#signupBox', { timeout: 3000 });
+		await page.type('#email', 'test@gmail.com');
+		await page.type('#name', 'testuser');
+		await page.type('#mobileNumber', '9876543210');
+		await page.type('#password', 'Test@123');
+		await page.type('#confirmPassword', 'Test@123');
+		await page.click('#submitButton');
+		await page.waitForNavigation();
+		await page.waitForSelector('#login', { timeout: 3000 });
+		console.log('TESTCASE:FE_Signup:success');
+		await login();
+	} catch (e) {
+		console.log('TESTCASE:FE_Signup:failure');
+	} finally {
+		await page.close();
+		await browser.close();
+	}
 })();
 
- 
+const login = async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	try {
+		await page.goto('http://localhost:8081/');
+		await page.setViewport({
+			width: 1200,
+			height: 800,
+		});
+		await page.type('#email', 'test@gmail.com');
+		await page.type('#password', 'Test@123');
+		await page.click('#loginButton');
+		await page.waitForNavigation();
+		await page.waitForSelector('#userHomeBox', { timeout: 3000 });
+		await page.click('#profileButton');
+		await page.waitForSelector('#userProfileBox', { timeout: 3000 });
+		console.log('TESTCASE:FE_login_userPage:success');
+	} catch (e) {
+		console.log('TESTCASE:FE_login_userPage:failure');
+	} finally {
+		await page.close();
+		await browser.close();
+	}
+};
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    try{
-    await page.goto('https://8081-dbcebacafbefccbefceeaecbbfabeaecfaaccbd.examlyiopb.examly.io/');
-    await page.setViewport({
-      width:1200,
-      height:800,
-    })
-      await page.type('#email', 'test@gmail.com');
-      await page.type('#password', 'Test@123');
-      await page.click('#loginButton');
-      await page.waitForNavigation();
-      await page.waitForSelector('#userHomeBox',{timeout:3000});
-      await page.click('#grid1');
-      await page.waitForSelector('#bookingArea',{timeout:3000});
-      await page.click('#profileButton');
-      await page.waitForSelector('#userProfileBox',{timeout:3000});
-      console.log('TESTCASE:FE_login_userPage:success');
-    }
-     catch(e){
-      console.log('TESTCASE:FE_login_userPage:failure');
-    }finally{
-      await page.close();
-      await browser.close();
-    }
-    
-})();
-
- 
-
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    try{
-    await page.goto('https://8081-dbcebacafbefccbefceeaecbbfabeaecfaaccbd.examlyiopb.examly.io/');
-    await page.setViewport({
-      width:1200,
-      height:800,
-    })
-      await page.type('#email', 'admin@gmail.com');
-      await page.type('#password', 'Admin@123');
-      await page.click('#loginButton');
-      await page.waitForNavigation();
-      await page.waitForSelector('#adminHome',{timeout:3000});
-      await page.click('#editButton');
-      await page.waitForSelector('#updateButton',{timeout:3000});
-      console.log('TESTCASE:FE_adminPage:success');
-    }
-     catch(e){
-      console.log('TESTCASE:FE_adminPage:failure');
-    }finally{
-      await page.close();
-      await browser.close();
-    }
-    
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	try {
+		await page.goto('http://localhost:8081/');
+		await page.setViewport({
+			width: 1200,
+			height: 800,
+		});
+		await page.type('#email', 'admin@gmail.com');
+		await page.type('#password', 'Admin@123');
+		await page.click('#loginButton');
+		await page.waitForNavigation();
+		await page.waitForSelector('#adminHome', { timeout: 3000 });
+		console.log('TESTCASE:FE_adminPage:success');
+	} catch (e) {
+		console.log('TESTCASE:FE_adminPage:failure');
+	} finally {
+		await page.close();
+		await browser.close();
+	}
 })();
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  try{
-  await page.goto('https://8081-dbcebacafbefccbefceeaecbbfabeaecfaaccbd.examlyiopb.examly.io/');
-  await page.setViewport({
-    width:1200,
-    height:800,
-  })
-    await page.type('#email', 'admin@gmail.com');
-    await page.type('#password', 'Admin@123');
-    await page.click('#loginButton');
-    await page.waitForNavigation();
-    await page.waitForSelector('#adminHome',{timeout:3000});
-    await page.click('#routeButton');
-    await page.waitForSelector('#adminRoute',{timeout:3000});
-    await page.click('#addButton');
-    await page.waitForSelector('#adminRoute',{timeout:3000});
-    await page.click('#logout');
-    await page.waitForSelector('#loginBox',{timeout:3000});
-    console.log('TESTCASE:FE_adminRoute:success');
-  }
-   catch(e){
-    console.log('TESTCASE:FE_adminRoute:failure');
-  }finally{
-    await page.close();
-    await browser.close();
-  }
-  
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	try {
+		await page.goto('http://localhost:8081/');
+		await page.setViewport({
+			width: 1200,
+			height: 800,
+		});
+		await page.type('#email', 'admin@gmail.com');
+		await page.type('#password', 'Admin@123');
+		await page.click('#loginButton');
+		await page.waitForNavigation();
+		await page.waitForSelector('#adminHome', { timeout: 3000 });
+		await page.click('#routeButton');
+		await page.waitForSelector('#adminRoute', { timeout: 3000 });
+		await page.click('#logout');
+		await page.waitForSelector('#login', { timeout: 3000 });
+		console.log('TESTCASE:FE_adminRoute:success');
+	} catch (e) {
+		console.log('TESTCASE:FE_adminRoute:failure');
+	} finally {
+		await page.close();
+		await browser.close();
+	}
 })();
